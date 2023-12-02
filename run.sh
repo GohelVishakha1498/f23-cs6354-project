785928617ab789a4bbe8ec9cdb326a3e2a2aeb23
GohelVishakha1498 <gohelvishakha@gmail.com>
2023-12-02 16:46:02 -0500

    run.sh update

--- run.sh
+++ run.sh
@@ -1,16 +1,6 @@
 python3 ./code/sim.py random 16384 16 128 > ./results/random_16384_16_128.txt
 python3 ./code/sim.py lfu 16384 16 64 > ./results/lfu_16384_16_64.txt
-python3 ./code/sim.py lru 16384 32 64 > ./results/lru_16384_32_64.txt
-
-python3 ./code/sim.py lfu 819 1 128 > ./results/lfu_819_1_128.txt
-python3 ./code/sim.py lfu 819 4 128 > ./results/lfu_819_4_128.txt
-python3 ./code/sim.py lfu 819 16 128 > ./results/lfu_819_16_128.txt
-python3 ./code/sim.py lfu 819 1 512 > ./results/lfu_819_1_512.txt
-python3 ./code/sim.py lfu 819 4 512 > ./results/lfu_819_4_512.txt
-python3 ./code/sim.py lfu 819 16 512 > ./results/lfu_819_16_512.txt
-python3 ./code/sim.py lfu 819 1 4096 > ./results/lfu_819_1_4096.txt
-python3 ./code/sim.py lfu 819 4 4096 > ./results/lfu_819_4_4096.txt
-python3 ./code/sim.py lfu 819 16 4096 > ./results/lfu_819_16_4096.txt
+python3 ./code/sim.py lru 16384 16 64 > ./results/lru_16384_32_64.txt
 
 python3 ./code/sim.py lfu 16384 1 128 > ./results/lfu_16384_1_128.txt
 python3 ./code/sim.py lfu 16384 4 128 > ./results/lfu_16384_4_128.txt
@@ -18,9 +8,9 @@ python3 ./code/sim.py lfu 16384 16 128 > ./results/lfu_16384_16_128.txt
 python3 ./code/sim.py lfu 16384 1 512 > ./results/lfu_16384_1_512.txt
 python3 ./code/sim.py lfu 16384 4 512 > ./results/lfu_16384_4_512.txt
 python3 ./code/sim.py lfu 16384 16 512 > ./results/lfu_16384_16_512.txt
-python3 ./code/sim.py lfu 16384 1 4096 > ./results/lfu_16384_1_4096.txt
-python3 ./code/sim.py lfu 16384 4 4096 > ./results/lfu_16384_4_4096.txt
-python3 ./code/sim.py lfu 16384 16 4096 > ./results/lfu_16384_16_4096.txt
+python3 ./code/sim.py lfu 16384 1 2048 > ./results/lfu_16384_1_2048.txt
+python3 ./code/sim.py lfu 16384 4 2048 > ./results/lfu_16384_4_2048.txt
+python3 ./code/sim.py lfu 16384 16 2048 > ./results/lfu_16384_16_2048.txt
 
 python3 ./code/sim.py lfu 32768 1 128 > ./results/lfu_32768_1_128.txt
 python3 ./code/sim.py lfu 32768 4 128 > ./results/lfu_32768_4_128.txt
@@ -28,6 +18,46 @@ python3 ./code/sim.py lfu 32768 16 128 > ./results/lfu_32768_16_128.txt
 python3 ./code/sim.py lfu 32768 1 512 > ./results/lfu_32768_1_512.txt
 python3 ./code/sim.py lfu 32768 4 512 > ./results/lfu_32768_4_512.txt
 python3 ./code/sim.py lfu 32768 16 512 > ./results/lfu_32768_16_512.txt
-python3 ./code/sim.py lfu 32768 1 4096 > ./results/lfu_32768_1_4096.txt
-python3 ./code/sim.py lfu 32768 4 4096 > ./results/lfu_32768_4_4096.txt
-python3 ./code/sim.py lfu 32768 16 4096 > ./results/lfu_32768_16_4096.txt
+python3 ./code/sim.py lfu 32768 1 2048 > ./results/lfu_32768_1_2048.txt
+python3 ./code/sim.py lfu 32768 4 2048 > ./results/lfu_32768_4_2048.txt
+python3 ./code/sim.py lfu 32768 16 2048 > ./results/lfu_32768_16_2048.txt
+
+python3 ./code/sim.py lru 16384 1 128 > ./results/lfu_16384_1_128.txt
+python3 ./code/sim.py lru 16384 4 128 > ./results/lfu_16384_4_128.txt
+python3 ./code/sim.py lru 16384 16 128 > ./results/lfu_16384_16_128.txt
+python3 ./code/sim.py lru 16384 1 512 > ./results/lfu_16384_1_512.txt
+python3 ./code/sim.py lru 16384 4 512 > ./results/lfu_16384_4_512.txt
+python3 ./code/sim.py lru 16384 16 512 > ./results/lfu_16384_16_512.txt
+python3 ./code/sim.py lru 16384 1 2048 > ./results/lfu_16384_1_2048.txt
+python3 ./code/sim.py lru 16384 4 2048 > ./results/lfu_16384_4_2048.txt
+python3 ./code/sim.py lru 16384 16 2048 > ./results/lfu_16384_16_2048.txt
+
+python3 ./code/sim.py lru 32768 1 128 > ./results/lfu_32768_1_128.txt
+python3 ./code/sim.py lru 32768 4 128 > ./results/lfu_32768_4_128.txt
+python3 ./code/sim.py lru 32768 16 128 > ./results/lfu_32768_16_128.txt
+python3 ./code/sim.py lru 32768 1 512 > ./results/lfu_32768_1_512.txt
+python3 ./code/sim.py lru 32768 4 512 > ./results/lfu_32768_4_512.txt
+python3 ./code/sim.py lru 32768 16 512 > ./results/lfu_32768_16_512.txt
+python3 ./code/sim.py lru 32768 1 2048 > ./results/lfu_32768_1_2048.txt
+python3 ./code/sim.py lru 32768 4 2048 > ./results/lfu_32768_4_2048.txt
+python3 ./code/sim.py lru 32768 16 2048 > ./results/lfu_32768_16_2048.txt
+
+python3 ./code/sim.py random 16384 1 128 > ./results/lfu_16384_1_128.txt
+python3 ./code/sim.py random 16384 4 128 > ./results/lfu_16384_4_128.txt
+python3 ./code/sim.py random 16384 16 128 > ./results/lfu_16384_16_128.txt
+python3 ./code/sim.py random 16384 1 512 > ./results/lfu_16384_1_512.txt
+python3 ./code/sim.py random 16384 4 512 > ./results/lfu_16384_4_512.txt
+python3 ./code/sim.py random 16384 16 512 > ./results/lfu_16384_16_512.txt
+python3 ./code/sim.py random 16384 1 2048 > ./results/lfu_16384_1_2048.txt
+python3 ./code/sim.py random 16384 4 2048 > ./results/lfu_16384_4_2048.txt
+python3 ./code/sim.py random 16384 16 2048 > ./results/lfu_16384_16_2048.txt
+
+python3 ./code/sim.py random 32768 1 128 > ./results/lfu_32768_1_128.txt
+python3 ./code/sim.py random 32768 4 128 > ./results/lfu_32768_4_128.txt
+python3 ./code/sim.py random 32768 16 128 > ./results/lfu_32768_16_128.txt
+python3 ./code/sim.py random 32768 1 512 > ./results/lfu_32768_1_512.txt
+python3 ./code/sim.py random 32768 4 512 > ./results/lfu_32768_4_512.txt
+python3 ./code/sim.py random 32768 16 512 > ./results/lfu_32768_16_512.txt
+python3 ./code/sim.py random 32768 1 2048 > ./results/lfu_32768_1_2048.txt
+python3 ./code/sim.py random 32768 4 2048 > ./results/lfu_32768_4_2048.txt
+python3 ./code/sim.py random 32768 16 2048 > ./results/lfu_32768_16_2048.txt
